f142826a755cd13d3df3d8cbbac985816cc64fa0
Harry <harryjp1995@gmail.com>
2018-10-14 17:40:32 +0100

    refactor: Removes old test

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -8,14 +8,6 @@ import org.junit.Test;
 public class GildedRoseTest {
 
   @Test
-  public void foo() {
-    Item[] items = new Item[] { new Item("foo", 0, 0) };
-    GildedRose app = new GildedRose(items);
-    app.updateQuality();
-    assertEquals("foo", app.items[0].name);
-  }
-
-  @Test
   public void notPastSellIn() {
     Item[] items = new Item[] { new Item("foo", 1, 10) };
     GildedRose app = new GildedRose(items);
