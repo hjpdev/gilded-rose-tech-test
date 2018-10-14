d0e516feb47ad9ded437108905f75e13bdabb38a
Harry <harryjp1995@gmail.com>
2018-10-14 17:36:49 +0100

    test: Adds more tests following requirements laid out in README

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -40,6 +40,14 @@ public class GildedRoseTest {
   }
 
   @Test
+  public void noQualityOver50() {
+    Item[] items = new Item[] { new Item("Aged Brie", 0, 50) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertFalse(app.items[0].quality > 50);
+  }
+
+  @Test
   public void agedBrieQualityWithoutSellIn() {
     Item[] items = new Item[] { new Item("Aged Brie", 0, 10) };
     GildedRose app = new GildedRose(items);
@@ -53,5 +61,13 @@ public class GildedRoseTest {
     GildedRose app = new GildedRose(items);
     app.updateQuality();
     assertEquals(11, app.items[0].quality);
+  }
+
+  @Test
+  public void sulfurasTest() {
+    Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 50) };
+    GildedRose app = new GildedRose(items);
+    app.updateQuality();
+    assertEquals(50, app.items[0].quality);
   }
 }
