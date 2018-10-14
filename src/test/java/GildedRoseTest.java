6b6044cbbcfa0a6ece605915f9ae18d23d5f0036
Harry <harryjp1995@gmail.com>
2018-10-14 18:05:17 +0100

    refactor/style: Refactors & styles tests

--- src/test/java/GildedRoseTest.java
+++ src/test/java/GildedRoseTest.java
@@ -7,91 +7,108 @@ import org.junit.Test;
 
 public class GildedRoseTest {
 
+  private GildedRose generateApp(String name, int days, int quality) {
+    Item[] items = new Item[] { new Item(name, days, quality) };
+    GildedRose app = new GildedRose(items);
+    return app;
+  };
+
   @Test
   public void notPastSellIn() {
-    Item[] items = new Item[] { new Item("foo", 1, 10) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("foo", 1, 10);
+
     app.updateQuality();
+
     assertEquals(9, app.items[0].quality);
   }
 
   @Test
   public void pastSellIn() {
-    Item[] items = new Item[] { new Item("foo", 0, 10) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("foo", 0, 10);
+
     app.updateQuality();
+
     assertEquals(8, app.items[0].quality);
   }
 
   @Test
   public void noNegativeQuality() {
-    Item[] items = new Item[] { new Item("foo", 0, 0) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("foo", 0, 0);
+
     app.updateQuality();
+
     assertFalse(app.items[0].quality < 0);
   }
 
   @Test
   public void noQualityOver50() {
-    Item[] items = new Item[] { new Item("Aged Brie", 0, 50) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("Aged Brie", 0, 50);
+
     app.updateQuality();
+
     assertFalse(app.items[0].quality > 50);
   }
 
   @Test
   public void agedBrieQualityWithoutSellIn() {
-    Item[] items = new Item[] { new Item("Aged Brie", 0, 10) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("Aged Brie", 0, 10);
+
     app.updateQuality();
+
     assertEquals(12, app.items[0].quality);
   }
 
   @Test
   public void agedBrieQualityWithSellIn() {
-    Item[] items = new Item[] { new Item("Aged Brie", 10, 10) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("Aged Brie", 10, 10);
+
     app.updateQuality();
+
     assertEquals(11, app.items[0].quality);
   }
 
   @Test
   public void sulfurasTest() {
-    Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 50) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("Sulfuras, Hand of Ragnaros", 0, 50);
+
     app.updateQuality();
+
     assertEquals(50, app.items[0].quality);
   }
 
   @Test
   public void backstagePasses() {
-    Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 12, 10) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("Backstage passes to a TAFKAL80ETC concert", 12, 10);
+
     app.updateQuality();
+
     assertEquals(11, app.items[0].quality);
   }
 
   @Test
   public void backstagePassesTenDays() {
-    Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 9, 10) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("Backstage passes to a TAFKAL80ETC concert", 9, 10);
+
     app.updateQuality();
+
     assertEquals(12, app.items[0].quality);
   }
 
   @Test
   public void backstagePassesFiveDays() {
-    Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 4, 10) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("Backstage passes to a TAFKAL80ETC concert", 4, 10);
+
     app.updateQuality();
+
     assertEquals(13, app.items[0].quality);
   }
 
   @Test
   public void backstagePassesAfterConcert() {
-    Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) };
-    GildedRose app = new GildedRose(items);
+    GildedRose app = generateApp("Backstage passes to a TAFKAL80ETC concert", 0, 10);
+
     app.updateQuality();
+
     assertEquals(0, app.items[0].quality);
   }
 }
